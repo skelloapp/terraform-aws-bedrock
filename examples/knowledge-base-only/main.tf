146660d319703bb87ea47e866553d73ea34dea5c
Alexa Perlov <aperlov@amazon.com>
2024-11-13 12:42:22 -0500

    adding create_agent boolean var

---
+++ examples/knowledge-base-only/main.tf
@@ -0,0 +1,32 @@
+#####################################################################################
+# Terraform module examples are meant to show an _example_ on how to use a module
+# per use-case. The code below should not be copied directly but referenced in order
+# to build your own root module that invokes this module
+#####################################################################################
+variable "region" {
+  type        = string
+  description = "AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "awscc" {
+  region = var.region
+}
+
+provider "opensearch" {
+  url         = module.bedrock.default_collection[0].collection_endpoint 
+  healthcheck = false
+}
+
+module "bedrock" {
+  source = "../.." # local example
+  create_kb = true
+  create_default_kb = true
+  create_agent = false
+  foundation_model = "anthropic.claude-v2"
+  instruction = "You are an automotive assisant who can provide detailed information about cars to a customer."
+}
