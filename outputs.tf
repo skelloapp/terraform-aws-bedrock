9b2f3e45be766cb7a671d76745c0601af0aad9ab
Alexa Perlov <a.perlov33@gmail.com>
2025-03-18 15:28:31 -0400

    fix: default collection output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "default_collection" {
-  value       = module.oss_knowledgebase[0].opensearch_serverless_collection
+  value       = length(module.oss_knowledgebase) > 0 ? module.oss_knowledgebase[0].opensearch_serverless_collection : null
   description = "Opensearch default collection value."
 }
 
